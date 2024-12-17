62d5a05bdd233a4c2f31be36c21bee090ffa0d37
dige1997 <nicolai.dn@gmail.com>
2024-12-18 00:15:51 +0300

    maindashboard api

--- app/routes/main-dashboard.jsx
+++ app/routes/main-dashboard.jsx
@@ -3,44 +3,47 @@ import DashboardData from "../components/DashboardData";
 import { NavLink, redirect } from "react-router-dom";
 import { authenticator } from "../services/auth.server";
 import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
 
 export const meta = () => {
   return [{ title: "Elevation" }];
 };
-export async function loader({ request }) {
+
+export const loader = async ({ request }) => {
+  const user = await authenticator.isAuthenticated(request, {
+    Redirect: "/main-dashboard",
+  });
+
   const openWeatherApiKey = process.env.OPEN_WEATHER_API_KEY;
 
-  const user = await authenticator.isAuthenticated(request);
-  if (!user) {
-    // User is not authenticated, return null or some default data
-    return { isAuthenticated: false };
-  }
-
-  // If authenticated, return the API keys and the flag
-  return {
-    isAuthenticated: true,
-    openWeatherApiKey,
-  };
-}
+  return json({
+    openWeatherApiKey, // API keys securely passed here
+    isAuthenticated: !!user, // determine authentication status
+  });
+};
 
 export default function MainDashboard() {
-  // Access the API keys from loader data
-  const { openWeatherApiKey } = useLoaderData();
+  const { openWeatherApiKey, isAuthenticated } = useLoaderData();
 
-  return (
-    <div className="page">
-      <div className="w-full top-0 bg-slate-200 h-14 flex justify-center items-center font-bold shadow-sm animate-slideDown z-10">
-        <p>
-          Sign in to see more or
-          <NavLink to="/signup" className="text-blue-600">
-            {" "}
-            sign up here
-          </NavLink>
-        </p>
-      </div>
+  if (!isAuthenticated) {
+    return (
       <div className="page">
-        <DashboardData openWeatherApiKey={openWeatherApiKey} />
+        <div className="w-full top-0 bg-slate-200 h-14 flex justify-center items-center font-bold shadow-sm animate-slideDown z-10">
+          <p>
+            Sign in to see more or
+            <NavLink to="/signup" className="text-blue-600">
+              {" "}
+              sign up here
+            </NavLink>
+          </p>
+        </div>
+        <div className="page">
+          <DashboardData apiKey={openWeatherApiKey} />{" "}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  // If the user is authenticated, redirect to the dashboard
+  return redirect("/dashboard");
 }
